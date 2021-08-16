7c95087a964352ea72350eef0f6751e26c92937a
neroAzsy12 <azael.zamora12@gmail.com>
2021-08-15 21:29:53 -0700

    Delete twostones.py

--- twostones.py
+++
@@ -1,6 +0,0 @@
-n = int(input())    # the number of stones, 1 ≤ N ≤ 10,000,000
-
-if (n % 2) != 0:    # if there are odd num of stones, Alice wins
-    print("Alice")
-else:               # if there are even num of stones, Bob wins
-    print("Bob")
